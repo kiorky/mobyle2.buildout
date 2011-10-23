effd88affadf331ac89bdb1f6ffe5f4d3d1a6dce
kiorky <kiorky@cryptelium.net>
2011-10-23 19:08:29 +0200

    enhanced launcher

--- l.sh
+++ l.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
-ps aux|grep paster|awk '{print $2}'|xargs kill -9;./bin/paster serve etc/wsgi/instance.ini 
+
+while true;do
+    ps aux|grep paster|grep mobyle2|awk '{print $2}'|xargs kill -9
+    reset
+    ./bin/paster serve --reload etc/wsgi/instance.ini
+    echo;echo;echo
+    echo "--------------------------------------------------------------"
+    echo "              Press enter to restart"
+    echo "--------------------------------------------------------------"
+    read
+done           
 # vim:set et sts=4 ts=4 tw=80:
